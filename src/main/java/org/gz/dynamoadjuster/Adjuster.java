19f1b7c16301e1e7b3d2a108c1c67bf2139eeffc
gaoxiang.zeng <gaoxiang.zeng@ocado.com>
2016-01-04 14:46:43 +0000

    add hardlimit

--- src/main/java/org/gz/dynamoadjuster/Adjuster.java
+++ src/main/java/org/gz/dynamoadjuster/Adjuster.java
@@ -4,6 +4,7 @@ public class Adjuster {
     protected static final double MARGIN = 1.1;
     private Updater updater;
     private Reporter reporter;
+    private Double hardlimit;
 
     public Adjuster(Updater updater, Reporter reporter) {
         this.updater = updater;
@@ -16,7 +17,15 @@ public class Adjuster {
 
         if (currentProvisionThroughput > consumedThroughput * MARGIN
                 || currentProvisionThroughput < consumedThroughput * MARGIN) {
-            updater.updateThroughput(consumedThroughput * MARGIN);
+            if (hardlimit != null) {
+                updater.updateThroughput(Math.min(consumedThroughput * MARGIN, hardlimit));
+            } else {
+                updater.updateThroughput(consumedThroughput * MARGIN);
+            }
         }
+    }
+
+    public void setHardlimit(double hardlimit) {
+        this.hardlimit = hardlimit;
     }
 }
--- src/test/java/org/gz/dynamoadjuster/AdjusterTest.java
+++ src/test/java/org/gz/dynamoadjuster/AdjusterTest.java
@@ -52,4 +52,19 @@ public class AdjusterTest {
 
         assertEquals(consumed * Adjuster.MARGIN, argument.getValue(), 0.00001);
     }
+
+    @Test
+    public void testCanOnlyIncreaseToHardlimit() throws Exception {
+        double hardlimit = 10.0;
+        adjuster.setHardlimit(hardlimit);
+        when(reporter.getReadProvisionThroughput()).thenReturn(5.0);
+        double consumed = 20.0;
+        when(reporter.getConsumedReadThroughput()).thenReturn(consumed);
+        ArgumentCaptor<Double> argument = ArgumentCaptor.forClass(double.class);
+
+        adjuster.action();
+        verify(updater, times(1)).updateThroughput(argument.capture());
+
+        assertEquals(hardlimit, argument.getValue(), 0.00001);
+    }
 }
